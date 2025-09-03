dd654332f79f8d63be25f601bc488969fc80e3bc
Maddy <omniversalsavior@gmail.com>
2025-09-03 23:39:28 +0530

    Resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import './App.css';
 import Chat from './Chat';
->>>>>>> b3316f318bb02a1dcbcb2a1c5f0c2779c0f6112f
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
     <Chat />
   </React.StrictMode>
 );
->>>>>>> b3316f318bb02a1dcbcb2a1c5f0c2779c0f6112f
